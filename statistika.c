355fb24569e93a106536392f324f749e9c298101
Thafa Fauzanli <86725385+thafafauzanli@users.noreply.github.com>
2022-07-16 20:24:19 +0700

    Add files via upload

---
+++ statistika.c
@@ -0,0 +1,16 @@
+/*
+ * 	STATISTIKA CLI (C) 2022
+ *	Penulis : Thafa Fauzanli
+ * 	Youtube : youtube.com/c/takuniinfinity 
+*/
+#include "src/kepala.h"
+
+int main() {
+	menu();
+}
+
+// Table Layout
+// +------------------+--------------+
+// |     Interval     | Frekuensi(f) |
+// +------------------+--------------+
+// |     60 - 64      |      50      |
